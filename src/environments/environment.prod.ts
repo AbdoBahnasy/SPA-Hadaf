3b350fe782b9486ab00d197f2d1c0222b21adcd2
Mahmoud Hassan <m.neosoft@hotmail.com>
2022-01-20 16:49:54 +0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
-export const environment = {
-  production: true,
-  apiUrl: 'https://sehatydbapi.2p-ccbu.com/api/v1',
-  mainURL: 'https://sehatydbid.2p-ccbu.com',
-  signalRserver : "https://sehatydbsi.2p-ccbu.com"
-};
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://sehatydbapi.2p-ccbu.com/api/v1',
+//   mainURL: 'https://sehatydbid.2p-ccbu.com',
+//   signalRserver : "https://sehatydbsi.2p-ccbu.com"
+// };
